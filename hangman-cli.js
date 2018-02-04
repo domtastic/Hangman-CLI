df8d3ee7303af16aff5cc73f26feaa31af2e976d
Dominic <gonza362@gmail.com>
2018-02-03 17:00:54 -0700

    initial

---
+++ hangman-cli.js
@@ -0,0 +1,17 @@
+// require inquirer
+const inquirer = require('inquirer');
+// requires the word and letter JS files
+const Word = require('./word.js');
+const Letter = require('./letter.js');
+
+// constructor of hangman game
+
+
+
+
+
+
+
+
+
+
